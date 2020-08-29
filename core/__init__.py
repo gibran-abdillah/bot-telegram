d2937003ce103390c26321f42283837fabbbfb76
gibran abdillah <70421698+gibran-abdillah@users.noreply.github.com>
2020-08-29 11:47:47 +0700

    Update Token

--- core/__init__.py
+++ core/__init__.py
@@ -7,6 +7,6 @@ import requests,os,json
 
 # configuration
 token = os.environ['TOKEN'] # dont remove this 
-my_url = 'https://api.telegram.org/bot1351102221:AAH-RVuQKzF0-XRIrZckxe0BgQwjUEuyHxQ'
-bot_token = 'bot1351102221:AAH-RVuQKzF0-XRIrZckxe0BgQwjUEuyHxQ'
+my_url = 'https://api.telegram.org/bottoken'
+bot_token = 'bottoken'
 app = Flask(__name__)
